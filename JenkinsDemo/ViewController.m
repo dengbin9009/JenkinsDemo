4605e99e9efd2b7c236ed078e08092aef89d7807
风扬 <fengyang@mytongche.com>
2019-01-10 15:13:02 +0800

    init

---
+++ JenkinsDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JenkinsDemo
+//
+//  Created by DengBin on 2019/1/10.
+//  Copyright © 2019 DengBin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+inhibit_all_warnings!
+target 'JenkinsDemo' do
+    
+    #net
+    pod 'AFNetworking', '~> 3.0'
+    
+    #model
+    pod 'JSONModel'
+
+end
